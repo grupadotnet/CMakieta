4427e81aaa40cd8a037b2e6f7b1068249f4ee59a
Czcibor Bohusz-Dobosz <czbd@o2.pl>
2022-12-20 07:59:40 +0100

    Add the code

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main(void) {
+	InitWindow(640, 480, "Nazwa okna");
+	
+	while(!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(/*RAYWHITE == */ (Color) {245, 245, 245, 255});
+		DrawText("Witaj, powiecie!", 0, 0, 48, BLACK); //TODO zrobić, żeby "świecie" działało... Z taką czcionką chyba się nie uda; DrawTextEx()?
+		EndDrawing();
+	}
+	
+	CloseWindow();
+	
+	return 0;
+}
